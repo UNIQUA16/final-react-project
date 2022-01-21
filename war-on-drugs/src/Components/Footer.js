5c06d01b8b111e3a5d23951ce2e4df804e1f9fa8
UNIQUA ROBINSON <urobinson@mec.science>
2022-01-21 16:58:05 -0500

    added a text file as well as began working on the footer of the page

--- war-on-drugs/src/Components/Footer.js
+++ war-on-drugs/src/Components/Footer.js
@@ -1,11 +1,24 @@
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
 
 function Footer({}) {
     
 
     return (
-        <div>
-        
-    </div>
+        <footer>
+            <Box>
+                <Container maxWidth ="lg">
+                <Grid container spacing={5}>
+                    <Grid item xs={4} sm={4}>
+                        <Box borderBottom={1}>Contact Us </Box>
+                        <Box></Box>
+                    </Grid>
+                </Grid>
+                </Container>
+            </Box>
+        </footer>
     );
 }
 
