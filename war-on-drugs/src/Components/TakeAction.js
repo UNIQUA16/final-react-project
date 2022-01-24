ffd5655abe6e89f3bccd4f2626e6b3354847974b
MigeulEliotte <m.eliotte18@gmail.com>
2022-01-24 12:50:26 -0500

    updated 1/24/2022 added more code and a better layout

--- war-on-drugs/src/Components/TakeAction.js
+++ war-on-drugs/src/Components/TakeAction.js
@@ -1,11 +1,24 @@
+import { Container } from "@mui/material";
+import Button from '@mui/material/Button';
 
 function Takeaction({}) {
-    
+
 
     return (
-        <div>
+    <Container>
+    <div className= "Take_Action">
+        <Button>
+            <h1 >
+                Take Action
+            </h1>
+            <p>
+                
+            Sign UP!
+            </p>
+        </Button>
+        </div>
         
-    </div>
+    </Container>
     );
 }
 
