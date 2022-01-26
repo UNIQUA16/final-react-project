d31863130f8716fd9e7cca513441a4a7f8e5b073
MigeulEliotte <m.eliotte18@gmail.com>
2022-01-26 15:53:58 -0500

    body update

--- war-on-drugs/src/Components/Body.js
+++ war-on-drugs/src/Components/Body.js
@@ -4,10 +4,26 @@ function WebBody({}) {
     
 
     return (
-        <div className= "WebBody">
+    <div className= "WebBody">
+          <h1>
+            IS IT JUSTICE OR “JUST-US” ?
+          </h1>
         <p1>
-        The Drug Policy Alliance (DPA) is the leading organization in the U.S., promoting alternatives to the war on drugs. We envision a just society in which the use and regulation of drugs are grounded in science, compassion, health, and human rights; in which people are no longer punished for what they put into their own bodies; and in which the fears, prejudices, and punitive prohibitions of today are no more.Since 2000, we led the way on creating cutting-edge policies that have fundamentally transformed the direction of drug policy in the U.S. and beyond. This includes a successful and growing marijuana reform movement across the nation. We are leading the movement to treat drug use as a health issue, not a criminal problem. In 2020, we made history in Oregon by decriminalizing drugs for the first time ever in the United States, and dramatically expanding health services in the state.DPA has grown to more than 40 staff across the U.S.; an annual operating budget of approximately $12 million, 12,000 dues-paying members and more than 170,000 online subscribers.
-      </p1>
+          The war on drugs brought about policies that are being misused for the incarceration and imprisonment of the black community. According to the Drug Policy Alliance nearly 80% of people in federal prison and almost 60% of people in the state prison for drug offenses are black or latino. The drug policies were not created to help the low income communities, they were created to tear their families a part. As a result of the discrimination against blacks and the misuse of the war on drugs, came about policies that would end drug offenders simple humans rights. A few consequences faced by drug offenders are policies that denied child custody, voting rights, employment, business loans, etc. These policies were clearly created with the intent to keep black people from progressing. Individuals who were not native Americans or born here would face harsh consequences when caught using drugs, deportation and detention being a part of the list. As a nation we must  hope to be successful at such task as we advocate for the legal system to decriminalize drug possession, make changes to the policies that will remove the harsh sentencing, provide diversity programs and so on.  
+        </p1>
+      <h5>  
+          THE DRUG WAR DRIVES RACIAL DISPARITY IN THE CRIMINAL LEGAL SYSTEM
+      </h5>
+          <p>
+            The war on drugs brought about policies that are being misused for the incarceration and imprisonment of the black community. According to the Drug Policy Alliance nearly 80% of people in federal prison and almost 60% of people in the state prison for drug offenses are black or latino. The drug policies were not created to help the low income communities, they were created to tear their families a part. As a result of the discrimination against blacks and the misuse of the war on drugs, came about policies that would end drug offenders simple humans rights. A few consequences faced by drug offenders are policies that denied child custody, voting rights, employment, business loans, etc. These policies were clearly created with the intent to keep black people from progressing. Individuals who were not native Americans or born here would face harsh consequences when caught using drugs, deportation and detention being a part of the list. As a nation we must  hope to be successful at such task as we advocate for the legal system to decriminalize drug possession, make changes to the policies that will remove the harsh sentencing, provide diversity programs and so on.
+          </p>
+
+          <h3>
+          UNJUST CONSEQUENCES FROM THE WAR ON DRUGS CAMPAIGN
+          </h3>
+          <p>
+          <b>Blacks comprised 43% and Whites 55% of persons convicted of drug felonies in state courts; Black people account for 53.5% and Whites for 33.3% of persons admitted to state prisons for new convictions for drug offenses. </b>
+        </p>
 
     </div>
     );
